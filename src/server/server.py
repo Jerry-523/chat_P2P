b52ccf52c99b7b35191ecf99c382b148236d9f59
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2024-04-22 11:09:32 -0100

    Atualizar o server.py

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import sqlite3
+from datetime import datetime
 
 class RequestHandler(BaseHTTPRequestHandler):
     def _set_response(self, status_code=200, headers={}):
@@ -10,18 +12,21 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_response()
-        self.wfile.write(json.dumps(messages).encode('utf-8'))
+        self.wfile.write(json.dumps(self.get_messages()).encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
         message_json = json.loads(post_data)
         message = message_json.get('message', '')
-        
+
+        client_ip = self.client_address[0]
+        self.save_message(client_ip, message)
+
         self._set_response()
-        self.wfile.write('Servidor: Recebi sua mensagem: {}'.format(message).encode('utf-8'))
-        messages.append(message)
-        print('{} ~#: {}'.format(self.client_address[0], message))
+        response_message = 'Servidor: Recebi sua mensagem: {}'.format(message)
+        self.wfile.write(response_message.encode('utf-8'))
+        print('{} ~#: {}'.format(client_ip, message))
 
     def do_OPTIONS(self):
         self.send_response(200)
@@ -30,10 +35,26 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
 
+    def save_message(self, client_ip, message):
+        connection = sqlite3.connect('messages.db')
+        cursor = connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS messages
+                          (client_ip TEXT, message TEXT, timestamp DATETIME)''')
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute("INSERT INTO messages VALUES (?, ?, ?)", (client_ip, message, timestamp))
+        connection.commit()
+        connection.close()
+
+    def get_messages(self):
+        connection = sqlite3.connect('messages.db')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM messages ORDER BY timestamp")
+        messages = [{'client_ip': row[0], 'message': row[1], 'timestamp': row[2]} for row in cursor.fetchall()]
+        connection.close()
+        return messages
+
 if __name__ == '__main__':
     port = 3000
-    messages = ['olaaaaaaaaaaa']
-
     server_address = ('', port)
     httpd = HTTPServer(server_address, RequestHandler)
     print('Servidor iniciado em http://localhost:{}'.format(port))
